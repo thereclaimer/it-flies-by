b0093262cf9d184a71049c2696874fe6fae65aa3
Sam Davenport <sam@davenportdronographics.com>
2024-08-18 11:39:32 -0500

    shader memory

--- src/engine/shader/ifb-engine-shader-memory.cpp
+++ src/engine/shader/ifb-engine-shader-memory.cpp
@@ -6,6 +6,13 @@ internal void
 ifb_engine_shader::memory_reserve(
     void) {
 
+    //reserve the memory
+    const size_t shader_memory_reservation_size = ifb_engine_memory_megabytes(64); 
+    
+    ifb_engine_memory::reserve_memory(
+        "SHADER MANAGER"
+        ,shader_memory_reservation_size)
+
 }
 
 internal void
